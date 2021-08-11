55f99413fe3e98aa752f1a3472e757c35785ad1c
Rahul-singh98 <rajput.rahul8510@gmail.com>
2021-08-11 23:19:34 +0530

    arrfill

--- Array_Filling.cpp
+++ Array_Filling.cpp
@@ -2,10 +2,6 @@
 #define ll long long int
 using namespace std;
 
-bool cmp(pair<ll ,ll>& a ,pair<ll ,ll>& b){
-    return a.first > b.first;
-}
-
 ll solve(ll arr[] , ll x , ll y , ll n ){
     ll sum=0;
     for(ll i=1;i<n;i++){
@@ -21,20 +17,19 @@ ll solve(ll arr[] , ll x , ll y , ll n ){
 }
 
 int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     int t;cin >> t;
     while(t--){
         ll n , m ; cin >> n >> m;
         ll arr[n+1];
-        map<ll , ll> mp;
+        map<ll , ll , greater<ll>> mp;
         memset(arr , 0 , sizeof(arr));
         ll sum=0;
         for(ll i=0;i<m;i++){
             ll x ,y; cin >> x >> y;
             mp[x]=y;
         }
-        sort(mp.begin() , mp.end() );
 
         for (auto i:mp){
             sum += solve(arr , i.first , i.second , n+1);
