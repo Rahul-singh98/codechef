11bc50735241e608f99c351c83837fb71bdf7549
Rahul-singh98 <rajput.rahul8510@gmail.com>
2021-08-30 23:56:57 +0530

    wormhole

--- Wormholes.cpp
+++ Wormholes.cpp
@@ -2,28 +2,35 @@
 using namespace std;
 
 int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
     int n, x , y ; cin >> n >> x >> y;
-    map<int ,int> t;
+    vector<pair<int ,int>> t;
     for(int i=0;i<n;i++){
         int a , b;cin >> a >> b;
-        t.insert(make_pair(a , b));
+        t.push_back(make_pair(a , b));
     }
     vector<int> V(x) , W(y);
     for(int i=0;i<x;i++)cin >> V[i];
     for(int i=0;i<y;i++)cin >> W[i];
 
-    sort(V.begin() ,V.end() , greater<int>());
-    sort(W.begin() , W.end() , greater<int>());
+    sort(V.begin() ,V.end());
+    sort(W.begin() , W.end());
+    sort(t.begin(),t.end());
 
-    for(auto itr:t){
-        for(int i=0;i<x;i++){
-            for(int j=0;j<y;j++){
-                if((V[i]<=itr.first) && (W[j]>=itr.second)){
-                    cout << W[j] - V[i] + 1 << endl;
-                }
-            }
-        }
-        cout << "\n";
-    }
+    int prev_min = INT_MAX;
+
+   for(int i=0;i<n;++i){
+       auto start = upper_bound(V.begin() , V.end() , t[i].first);
+       auto end = upper_bound(W.begin() , W.end() , t[i].second);
+       
+        cout << *start << " " << *end << endl;
+       if (start!=V.begin() && end!=W.end()){
+           --start;
+           prev_min = min(prev_min , *end - *start +1);
+       }
+   }
+    cout << prev_min << '\n';
     return 0;
 }
