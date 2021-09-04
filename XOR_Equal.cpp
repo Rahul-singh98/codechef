7093b5c81779f6a782a704005e48a5a48e107e3b
Rahul-singh98 <rajput.rahul8510@gmail.com>
2021-09-05 01:10:30 +0530

    unsolved PALINT

--- XOR_Equal.cpp
+++ XOR_Equal.cpp
@@ -9,20 +9,25 @@ int main(){
         vector<ll> a;
         map<ll , ll> valCounts;
         set<ll> uVals;
+        bool cvt=true;
         for(int i=0;i<n;i++){
             int y; cin >>y ;
             a.push_back(y);
             uVals.insert(y);
             valCounts[y]++;
         }
-        for(auto w:valCounts)
+        for(auto w:valCounts){
             nMax = max(nMax , w.second);
-    
+        }
         for(auto w : uVals){
             int z = w ^ x;
             if (valCounts.find(z)!=valCounts.end()){
                 nMax = max(nMax , valCounts[z] + valCounts[w]);
-                op=valCounts[w];
+                if(cvt){
+                    op++;
+                    cvt=false;
+                }
+                op=min(op,valCounts[w]);
             }
         }
 
