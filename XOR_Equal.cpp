a8a9bc0dd7b97ae50c0d6556dbe0d7646ad674f2
Rahul-singh98 <rajput.rahul8510@gmail.com>
2021-09-04 16:45:32 +0530

    Sept Long Challenge

---
+++ XOR_Equal.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+int main(){
+    int t; cin >> t;
+    while(t--){
+        ll n ,x  ,nMax=0, op=0; cin >> n >> x;
+        vector<ll> a;
+        map<ll , ll> valCounts;
+        set<ll> uVals;
+        for(int i=0;i<n;i++){
+            int y; cin >>y ;
+            a.push_back(y);
+            uVals.insert(y);
+            valCounts[y]++;
+        }
+        for(auto w:valCounts)
+            nMax = max(nMax , w.second);
+    
+        for(auto w : uVals){
+            int z = w ^ x;
+            if (valCounts.find(z)!=valCounts.end()){
+                nMax = max(nMax , valCounts[z] + valCounts[w]);
+                op=valCounts[w];
+            }
+        }
+
+        cout << nMax << " " << op << endl;
+    }
+    return 0;
+}
