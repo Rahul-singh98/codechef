0af0abd8b42bf55f8f2ffce1825dac16bb3ff5a4
Rahul-singh98 <rajput.rahul8510@gmail.com>
2021-10-06 23:21:53 +0530

    yet another mex

--- Yet_another_MEX_problem.cpp
+++ Yet_another_MEX_problem.cpp
@@ -1,25 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int mex(vector<int> arr , int n){
+int mex(vector<int> arr , int n ,int start , int end){
     sort(arr.begin() , arr.end());
-    int curMex = arr[n];
-    for(int i=0;i<n;i++){
+    int curMex = 0;
+    for(int i=start;i<=end;i++){
         if(curMex == arr[i])
-            curMex--;
+            curMex++;
     }
-    if(curMex < arr[0])
-        curMex = arr[n-1]+1;
-
     return curMex;
 }
 
 int main(){
     int t; cin >>t;
-    vector<int> a(t);
-    for(int i=0;i<t;i++)
-        cin >> a[i];
+    while(t--){ 
+        int n , k ; cin >> n >> k;
+        vector<int> a(n) ,b;
+        for(int i=0;i<n;i++)
+            cin >> a[i];
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                b.push_back(mex(a , n , i , j));
+            }
+        }
+        sort(b.begin() , b.end());
+        // for(int i=0;i<b.size();i++)
+        //     cout << b[i] << ' ';
 
-    cout << mex(a , t);
+        cout<< b[k-1] << '\n';
+    }
     return 0;
 }
